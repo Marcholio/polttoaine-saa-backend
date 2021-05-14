66a29c48f6d21a2c7c80c810be568e13f5f03ca2
Markus <markus.tyrkko@gmail.com>
2021-05-14 22:41:06 +0300

    First working API

--- src/handler.ts
+++ src/handler.ts
@@ -1,6 +1,11 @@
 export const getStations = async () => {
   return {
     statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
     body: JSON.stringify([
       {
         station: "Auroranportti Kylänportti 16",
